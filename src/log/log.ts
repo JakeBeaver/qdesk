abc16d06ba351f79b8634f7872f8ed857f740e46
Jakub Kolezynski <jakub@kolezyn.ski>
2026-05-31 19:37:00 +0200

    change the "failed" icon to a warning

--- src/log/log.ts
+++ src/log/log.ts
@@ -7,7 +7,7 @@ export function logSwitch({
   name: string;
   type: "success" | "failed" | "missing";
 }) {
-  const status = type === "success" ? "✅" : type === "failed" ? "❌" : "❓";
+  const status = type === "success" ? "✅" : type === "failed" ? "⚠️" : "❓";
 
   console.log(`[switch ${chord}] ${status} "${name}"`);
 }
